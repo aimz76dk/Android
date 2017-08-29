6fa15394cdb8114b10081d6eb8a99a36decc6f2e
aimz <aimzbooking@gmail.com>
2017-08-29 14:21:30 +0200

    just some more

--- GameEngine/app/src/main/java/aimz76dk/gameengine/GameEngine.java
+++ GameEngine/app/src/main/java/aimz76dk/gameengine/GameEngine.java
@@ -2,6 +2,7 @@ package aimz76dk.gameengine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameEngine extends Activity {
 
@@ -9,6 +10,17 @@ public class GameEngine extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        //setContentView(R.layout.activity_main);
+    }
+
+    public void onPause() {
+        super.onPause();
+    }
+
+    public void onResume() {
+        super.onResume();
     }
 }
