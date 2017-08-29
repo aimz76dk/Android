defecebf21815ebb41984f7dd4c8da970a3e0a68
aimz <aimzbooking@gmail.com>
2017-08-29 15:21:33 +0200

    just some more

--- GameEngine/app/src/main/java/aimz76dk/gameengine/GameEngine.java
+++ GameEngine/app/src/main/java/aimz76dk/gameengine/GameEngine.java
@@ -2,10 +2,17 @@ package aimz76dk.gameengine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
-public class GameEngine extends Activity
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameEngine extends Activity implements Runnable
 {
+    private Thread mainLoopThread;
+    private State state = State.Paused;
+    private List<State> stateChanges = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,16 +22,52 @@ public class GameEngine extends Activity
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+
+
         // setContentView(R.layout.activity_main);
     }
 
     public void onPause()
     {
         super.onPause();
+
     }
 
     public void onResume()
     {
         super.onResume();
+    }
+
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            synchronized (stateChanges)
+            {
+                int stopValue = stateChanges.size();
+                for (int i = 0; i < stopValue; i++)
+                {
+                    state = stateChanges.get(i);
+                    if (state == State.Disposed)
+                    {
+                        Log.d("GameEngine", "Main Loop thread is disposed");
+                        return;
+                    }
+                    if (state == State.Paused)
+                    {
+                        Log.d("GameEngine", "Main Loop thread is paused");
+                        return;
+                    }
+                    if (state == State.Resume)
+                    {
+                        Log.d("GameEngine", "Main Loop thread is resumed");
+                        state = State.Running;
+                    }
+                }
+                stateChanges.clear();
+            }
+        }
+
     }
 }
---
+++ GameEngine/app/src/main/java/aimz76dk/gameengine/State.java
@@ -0,0 +1,9 @@
+package aimz76dk.gameengine;
+
+public enum State
+{
+    Running,
+    Paused,
+    Resume,
+    Disposed
+}
