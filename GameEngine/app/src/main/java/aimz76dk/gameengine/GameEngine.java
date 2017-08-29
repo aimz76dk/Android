d87cf9384f7c72c1c768cc45a80a33babf176d09
aimz <aimzbooking@gmail.com>
2017-08-29 14:01:22 +0200

    first commit

---
+++ GameEngine/app/src/main/java/aimz76dk/gameengine/GameEngine.java
@@ -0,0 +1,14 @@
+package aimz76dk.gameengine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameEngine extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
